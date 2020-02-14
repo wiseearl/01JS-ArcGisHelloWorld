279873d62a821c62b2de6d862305d0fe12199c7a
Neon <wiseearl@gmail.com>
2020-02-14 12:02:24 +0800

    first content

---
+++ app/src/main/java/com/mds/arcgis/MainActivity.java
@@ -0,0 +1,58 @@
+package com.mds.arcgis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    // *** ADD ***
+    private MapView mMapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // *** ADD ***
+        mMapView = findViewById(R.id.mapView);
+        setupMap();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mMapView != null) {
+            mMapView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mMapView != null) {
+            mMapView.dispose();
+        }
+        super.onDestroy();
+    }
+
+    private void setupMap() {
+        if (mMapView != null) {
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double latitude = 34.0270;
+            double longitude = -118.8050;
+            int levelOfDetail = 13;
+            ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+            mMapView.setMap(map);
+        }
+    }
+}
